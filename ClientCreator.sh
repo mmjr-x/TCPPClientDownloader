f1c980885c666466f59705d19cad0b3372dfb80c
Tom Dymel <td@wps.de>
2021-09-09 07:15:42 +0200

    Changing script to not patch in order to get a normal client

--- ClientCreator.sh
+++ ClientCreator.sh
@@ -24,22 +24,6 @@ mv WoW-15595/Data .
 rm -rf WoW-15595
 rm WoWRegeneration.exe
 
-echo "Patching clients..."
-chmod +x ./connection_patcher
-./connection_patcher Wow.exe
-./connection_patcher Wow-64.exe
-mv Wow_Patched.exe Wow.exe
-mv Battle.net_Patched.dll Battle.net.dll
-mv Wow-64_Patched.exe Wow-64.exe
-mv Battle.net-64_Patched.dll Battle.net-64.dll
-
-mkdir -p ~/.wine/drive_c/ProgramData/Blizzard\ Entertainment/Battle.net/Cache/
-cp -rf AuthModules/* ~/.wine/drive_c/ProgramData/Blizzard\ Entertainment/Battle.net/Cache/
-cp -rf AuthModules_64/* ~/.wine/drive_c/ProgramData/Blizzard\ Entertainment/Battle.net/Cache/
-rm -rf AuthModules
-rm -rf AuthModules_64
-rm connection_patcher
-
 echo "Cleaning up..."
 cd ../../
 mv ClientCreator/wow434 .
